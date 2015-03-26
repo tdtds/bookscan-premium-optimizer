3140f3f8ecb94791330ecea9c6d227d57a90934c
TADA Tadashi <t@tdtds.jp>
2015-03-26 12:00:55 +0900

    specify assets precompile files

--- bookscan-premium-optimizer.rb
+++ bookscan-premium-optimizer.rb
@@ -14,7 +14,7 @@ module BookscanPremiumOptimizer
 		set :haml, {format: :html5, escape_html: true}
 		enable :logging
 
-		set :assets_compile, true
+		set :assets_precompile, %w(application.js application.css *.png *.jpg *.svg)
 		register Sinatra::AssetPipeline
 		if defined?(RailsAssets)
 			RailsAssets.load_paths.each do |path|
